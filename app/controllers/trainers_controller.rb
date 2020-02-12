b52824f9f43964da11ac016b324cb0876e523852
github username <github email address>
2020-02-12 11:01:27 -0500

    pushing stuff

--- app/controllers/trainers_controller.rb
+++ app/controllers/trainers_controller.rb
@@ -1,2 +1,24 @@
 class TrainersController < ApplicationController
+    def new
+        @pokemons = Pokemon.all
+        @trainer = Trainer.new
+    end
+    
+    def create
+        @pokemons = Pokemon.all
+        @trainer = Trainer.create(trainer_params)
+    end
+
+    def destroy
+    end
+
+    private
+
+    def find_me
+        Trainer.find(params[:id])
+    end
+
+    def trainer_params
+        params.require(:trainer).permit(:name, :pokemon_ids)
+    end
 end
--- app/models/trainer.rb
+++ app/models/trainer.rb
@@ -1,4 +1,7 @@
 class Trainer < ApplicationRecord
     has_many :badges
     has_many :gymleaders, through: :badges
+
+    has_many :pokeballs
+    has_many :pokemons, through: :pokeballs
 end
