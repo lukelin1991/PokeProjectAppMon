05f42db0c125021c712fc0a8488b548fa9d348c4
github username <github email address>
2020-02-14 09:30:43 -0500

    put validates_uniqueness_of for badge gaining, in badge controller and models

--- app/controllers/badges_controller.rb
+++ app/controllers/badges_controller.rb
@@ -9,13 +9,19 @@ class BadgesController < ApplicationController
             return redirect_to new_login_path
         end
 
+
         if @logged_in_trainer.can_battle
             @badge = Badge.create(trainer_id: current_trainer_id, gymleader_id: params[:gymleader_id])
-            flash[:notification] = "Congratulations on defeating #{@badge.gymleader.name}!"
-            flash[:notification] = "Now go get yourself another pokemon!"
+            if !@badge.valid?
+                flash[:notification] = "You've already received this Gym Badge."
+                redirect_to pokeballs_path
+            else
+                flash[:notification] = "Congratulations on defeating #{@badge.gymleader.name}!"
+                redirect_to pokeballs_path
+            end
         else
             flash[:notification] = "You don't have enough Pokemon to battle this Gym Leader. Add a Pokemon to your party."
+            redirect_to root_path
         end
-        redirect_to pokemons_path
     end
 end
